029273f1a755f0353a437c118fd64582d9a26dc2
Branley Mmasi <mmasi.branley@gmail.com>
2022-01-31 21:50:56 -0500

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Branley Mmasi on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("Hello Branley!")
+        TextLabel.textColor = UIColor.orange
+    }
+}
+
